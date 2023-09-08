ecd8124498c8d822a9d83404425aa2c205939794
Farha Ahmed <farhaa853@gmail.com>
2023-09-08 04:30:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
